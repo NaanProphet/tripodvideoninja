a933f69a45e032ca904960ab08726e1650f7c855
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2023-09-15 11:00:20 -0400

    start script error handling
    making it easier post clone

--- start.sh
+++ start.sh
@@ -2,4 +2,14 @@
 
 # pass -d to this script to start the container in the background
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FILE="$SCRIPT_DIR/.env.dev"
+if [ ! -f "$FILE" ]; then
+    printf "Algolia file ${RED}.env.dev${NC} missing!\nTo setup: ${RED}echo ALGOLIA_API_KEY=[my_api_key] > .env.dev${NC}\n"
+	exit
+fi
+
 docker-compose up $@
